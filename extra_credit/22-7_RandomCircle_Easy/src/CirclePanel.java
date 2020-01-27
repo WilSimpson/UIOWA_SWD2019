a934cfaccb260969a748ae5de364c66a2a5a5b8c
uiowa <wsimpson@uiowa.edu>
2020-01-26 20:50:39 -0600

    Initial attempt at 22-7_RandomCircle_Easy. Everything seems to be working, need to check on school PC to ensure the modules are configured correctly and the program runs as intended.

---
+++ extra_credit/22-7_RandomCircle_Easy/src/CirclePanel.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This panel extends the JPanel and deals with painting the circle to the screen. A MyCircle object is required to
+ * construct this object. Once supplied the class overrides the paint and preferredSize to easily paint the given
+ * circle to a JFrame
+ *
+ * @author Wil Simpson
+ */
+public class CirclePanel extends JPanel {
+
+    /**
+     * Minimum distance circle will be from all directions to the edge of the panel
+     */
+    public static final int CIRCLE_EDGE_OFFSET = 10;
+
+    /**
+     * The width the of the panel
+     */
+    public static final int PANEL_WIDTH = 500;
+
+    /**
+     * The height of the panel
+     */
+    public static final int PANEL_HEIGHT = 500;
+
+    /**
+     * The unchanging MyCircle object that we will be using and drawing
+     */
+    private MyCircle circle;
+
+    /**
+     * The constructor creates an object with a given MyCircle and setups the working space for the panel
+     *
+     * @param circle the MyCircle object that is to be drawn
+     */
+    public CirclePanel(MyCircle circle) {
+        this.circle = circle;
+        setBounds(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
+        setBackground(Color.WHITE);
+    }
+
+    /**
+     * Overriding the preferred size to that of the exact size of the MyCircle object
+     *
+     * @return Dimension that exactly contains the circle
+     */
+    @Override
+    public Dimension getPreferredSize() {
+        //The dimension of the circle is the preferred size plus the minimum distance defined by CIRCLE_EDGE_OFFSET
+        //Value must be multiplied by two to calculate for all 4 sides of the circle
+        return new Dimension(circle.getRadius()+CIRCLE_EDGE_OFFSET*2, circle.getRadius()+CIRCLE_EDGE_OFFSET*2);
+    }
+
+    /**
+     * Overriding the paint method to draw our circle. The circle is draw from x:0,y:0 at the top left of the panel.
+     *
+     * @param g the graphics object required to draw our circle
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.drawOval(CIRCLE_EDGE_OFFSET, CIRCLE_EDGE_OFFSET, circle.getRadius(), circle.getRadius());
+    }
+}
---
+++ extra_credit/22-7_RandomCircle_Easy/src/MyCircle.java
@@ -0,0 +1,67 @@
+/**
+ * Creates a baseline for a circle. The constructor requires no parameters and creates a random radius within
+ * the margins defined by the MIN_RADIUS and the MAX_RADIUS. Other useful methods are created to easily calculate the
+ * properties of the circle given the randomly generated radius.
+ *
+ * @author Wil Simpson
+ */
+public class MyCircle {
+    /**
+     * The minimum radius the circle may be
+     */
+    public static final int MIN_RADIUS = 100;
+
+    /**
+     * The maximum radius the circle may be
+     */
+    public static final int MAX_RADIUS = 500;
+
+    /**
+     * The actual radius of the circle
+     */
+    private int radius;
+
+    /**
+     * Constructs the MyCircle object with no params. Randomly generates the circle inclusively between the MIN_RADIUS
+     * and the MAX_RADIUS.
+     */
+    public MyCircle() {
+        this.radius = (int) Math.floor(Math.random()*(MAX_RADIUS-MIN_RADIUS)) + MIN_RADIUS;
+    }
+
+    /**
+     * Returns the radius of the circle object
+     *
+     * @return the radius of the circle
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * Calculates the diameter of the circle following the equation: 2 * radius
+     *
+     * @return the diameter of the circle
+     */
+    public int getDiameter() {
+        return 2 * radius;
+    }
+
+    /**
+     * Calculates the area of the circle following the equation: pi * radius^2
+     *
+     * @return the area of the circle
+     */
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    /**
+     * Calculates the circumference of the circle
+     *
+     * @return the circumference of the circle
+     */
+    public double getCircumference() {
+        return Math.PI * 2 * radius;
+    }
+}
