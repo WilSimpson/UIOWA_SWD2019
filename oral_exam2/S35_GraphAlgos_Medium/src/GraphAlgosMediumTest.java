eb43c31466f9c5d8bf5cd38d3d064963d027f5ed
Wil Simpson <wsimpson@uiowa.edu>
2020-04-29 00:27:56 -0500

    Can't spell!

---
+++ oral_exam2/S35_GraphAlgos_Medium/src/GraphAlgosMediumTest.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class GraphAlgosMediumTest
+{
+    public static void main(String[] args)
+    {
+        GraphAlgos algos = new GraphAlgos("words.dat");
+        //GraphAlgos algos = new GraphAlgos("words-short.dat");
+        //GraphAlgos algos = new GraphAlgos("wordlist.dumb");
+        //System.out.println(algos.getGraph().getAverageNumEdges());
+
+        List<String> list;
+
+        System.out.print("BFS: ");
+        list = algos.getGraph().findLargestConnectedSetVertsBFS();
+        System.out.println(list);
+
+        System.out.print("DFS: ");
+        list = algos.getGraph().findLargestConnectedSetVertsDFS();
+        System.out.println(list);
+
+        //list = algos.getAllConnectedVerts(list);
+
+        System.out.println("Size of largest connected set: "+list.size());
+
+
+    }
+}
